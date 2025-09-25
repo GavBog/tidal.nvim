9734696d88dd171c5275bcefe6dba32edd3060a5
Thomas Grund <info@mrreason.org>
2025-09-25 14:17:14 +0200

    Adjust latency in BootTidal

--- bootfiles/BootTidal.hs
+++ bootfiles/BootTidal.hs
@@ -13,9 +13,10 @@ default (Rational, Integer, Double, Pattern String)
 
 -- tidalInst <- mkTidalWith [(superdirtTarget { oLatency = 0.01 }, [superdirtShape])] (defaultConfig {cFrameTimespan = 1/50, cProcessAhead = 1/20})
 -- let editorTarget = Target {oName = "editor", oAddress = "127.0.0.1", oPort = 6013, oLatency = 0.07, oSchedule = Pre BundleStamp, oWindow = Nothing, oHandshake = False, oBusPort = Nothing }
-let editorTarget = Target {oName = "editor", oAddress = "127.0.0.1", oPort = 6013, oLatency = 0.25, oSchedule = Live, oWindow = Nothing, oHandshake = False, oBusPort = Nothing }
+
+let editorTarget = Target {oName = "editor", oAddress = "127.0.0.1", oPort = 6013, oLatency = 0.03, oSchedule = Live, oWindow = Nothing, oHandshake = False, oBusPort = Nothing }
 let editorShape = OSCContext "/editor/highlights"
-tidalInst <- mkTidalWith [(superdirtTarget { oLatency = 0.2 }, [superdirtShape]), (editorTarget, [editorShape])] (defaultConfig {cFrameTimespan = 1/50, cProcessAhead = 1/20})
+tidalInst <- mkTidalWith [(superdirtTarget { oLatency = -0.02 }, [superdirtShape]), (editorTarget, [editorShape])] (defaultConfig {cFrameTimespan = 1/50, cProcessAhead = 1/20})
 
 -- This orphan instance makes the boot aliases work!
 -- It has to go after you define 'tidalInst'.
